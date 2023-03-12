e73032f899e22fab5f301fabe77fa45c2862f109
dmigo <d.f.goryunov@gmail.com>
2023-03-12 20:26:41 +0100

    Use rich

--- ui/loop.py
+++ ui/loop.py
@@ -1,9 +1,10 @@
-import click
+from rich.console import Console
 from dataclasses import dataclass
 from domain.team import Team, Day, Feature
 from typing import List
 from time import sleep
 
+console = Console()
 
 RULES = """
 How would you like to spend the next hour?
@@ -24,17 +25,17 @@ class TeamReporter:
     team: Team
 
     def report(self):
-        click.echo(f"## Current team ##")
+        console.print(f"## Current team ##")
         for member in self.team.members:
-            click.echo(f"\t{member.name}\t[skill: {member.skill}]")
-        click.echo("")
+            console.print(f"\t{member.name}\t[skill: {member.skill}]")
+        console.print("")
         features_reporter = FeaturesReporter(features=self.team.features)
         features_reporter.report()
-        click.echo("")
-        click.echo("## Current idleness ##")
-        click.echo("")
-        click.echo(self.team.idle_time())
-        click.echo("")
+        console.print("")
+        console.print("## Current idleness ##")
+        console.print("")
+        console.print(self.team.idle_time())
+        console.print("")
 
 
 @dataclass
@@ -42,12 +43,12 @@ class FeaturesReporter:
     features: List[Feature]
 
     def report(self):
-        click.echo(f"### Current work in progress ###")
+        console.print(f"### Current work in progress ###")
         for feature in self.features:
-            click.echo("")
-            click.echo(f"\t {feature.name}\t[{len(feature.tasks)}]")
+            console.print("")
+            console.print(f"\t {feature.name}\t[{len(feature.tasks)}]")
             for task in feature.tasks:
-                click.echo(f"\t {task.name}\t[{task.complexity} {task.progress}]")
+                console.print(f"\t {task.name}\t[{task.complexity} {task.progress}]")
 
 
 @dataclass
@@ -55,20 +56,18 @@ class DayReporter:
     day: Day
 
     def report(self):
-        click.echo("")
-        click.secho(f"# DAY {self.day.number} {self.day.current_time()} #", bold=True)
-        click.echo("")
+        console.print("")
+        console.print(
+            f"# DAY {self.day.number} {self.day.current_time()} #", style="bold"
+        )
+        console.print("")
         team_reporter = TeamReporter(team=self.day.team)
         team_reporter.report()
 
 
-def ellipsis():
-    sleep(1)
-    click.echo(".")
-    sleep(1)
-    click.echo(".", end="")
-    sleep(1)
-    click.echo(".", end="")
+def ellipsis(text: str = ""):
+    with console.status(text, spinner="aesthetic"):
+        sleep(2)
 
 
 class MainLoop:
@@ -80,16 +79,18 @@ class MainLoop:
         self.reporter = DayReporter(day=self.day)
 
     def run(self):
-        click.echo(RULES)
+        console.print(RULES)
 
         while True:
             action = input(PROMPT)
             if action == "1":
                 self.day.meet_pm()
                 self.reporter.report()
+                ellipsis("Meeting the PM ")
             elif action == "2":
                 self.day.plan()
                 self.reporter.report()
+                ellipsis("Planning the next feature")
             elif action == "3":
                 t1 = self.day.team.features[0].tasks[0]
                 t2 = self.day.team.features[0].tasks[1]
@@ -100,9 +101,10 @@ class MainLoop:
             elif action == "4":
                 self.day.work()
                 self.reporter.report()
+                ellipsis("Everyone is working hard")
             elif action == "q":
                 # TODO calculate score
-                click.secho("Fare well!", bold=True)
+                console.print("Fare well!", style="bold")
                 break
             else:
-                click.secho("No such action", bold=True)
+                console.print("No such action", style="bold")
